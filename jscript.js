c1d80b29c24648b052abaea6ab53c7c9d59fa38f
Vaysa <konaichevkardonnyj@mail.ru>
2021-09-28 22:45:40 +0300

    Repozitoriy

--- jscript.js
+++ jscript.js
@@ -3,14 +3,22 @@ let themeButton = document.querySelector('.theme-button');
 let pag = document.querySelector('.pag');
 let pa = document.querySelector('.pa');
 let paragraph = document.querySelector('p');
-
+let image = document.getElementById("image");
+//image.setAttribute('src', "files/10.jpg");
+//imgs = "files/10.jpg"
 themeButton.onclick = function() {
-    console.log('проверка')
-  page.classList.toggle('light-theme');
+      page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme'); 
   pag.classList.toggle('page-footer');
   pag.classList.toggle('page-foote'); 
   pa.classList.toggle('page-header');
   pa.classList.toggle('page-heade');
+  image.src="files/11.jpg";
+  //  image.src=imgs++;
 };
 console.log (paragraph.textContent);
+function my_click()
+{
+  document.getElementById("image").src="files/10.jpg";
+};
+
